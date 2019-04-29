56975deba3a4cf34b47321784e36ca58f3727312
Cedric Simon <5772359+simonced@users.noreply.github.com>
2019-04-29 20:56:54 +0900

    add a layer that is better for gaming

--- keyboards/ergodash/rev2/keymaps/simonced/keymap.c
+++ keyboards/ergodash/rev2/keymaps/simonced/keymap.c
@@ -20,6 +20,9 @@ extern keymap_config_t keymap_config;
 // selection layer
 #define _SE 4
 
+// gaming layer (will be a toggle)
+#define _GA 5
+
 // unused adjust layer
 #define _ADJUST 16
 
@@ -35,19 +38,20 @@ extern keymap_config_t keymap_config;
 
 // motion layer switch
 #define SW_ML LT(_MO, KC_F) // temporary
-#define T_MO TG(_MO) // toggle (unused)
 
 // symbols layer switch
 // #define SW_SL LT(_SY, KC_ENT) // temporary (a bit painful for the whole day...)
 #define SW_SLE LT(_SY, KC_E) // temporary (E on left hand)
 #define SW_SLI LT(_SY, KC_I) // temporary (I on right hand)
-#define T_SY TG(_SY) // toggle (unused)
 
 // win layer switch
-#define SW_WL LT(_WIN, KC_G)
+#define SW_WL LT(_WIN, KC_D)
 
 // text selection switch
 #define SW_SE LT(_SE, KC_V)
+
+// gaming later switch
+#define TG_GA TO(_GA) // toggle from mothion layer on G key (so it activates with F-G)
 
 // == SHORTCUTS RELATED ==
 
@@ -106,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
     KC_GRV , KC_1,    KC_2,  KC_3,   KC_4,  KC_5,    KC_MINS,                         KC_EQL,  KC_6,   KC_7,  KC_8,     KC_9,    KC_0,    KC_HOME, \
     KC_TAB , KC_Q,    KC_W,  SW_SLE, KC_R,  KC_T,    KC_LBRC,                         KC_RBRC, KC_Y,   KC_U,  SW_SLI,   KC_O,    KC_P,    KC_END,  \
-    CTR_ESC, KC_A,    KC_S,  KC_D,   SW_ML, SW_WL,   KC_DEL,                          KC_BSPC, KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
+    CTR_ESC, KC_A,    KC_S,  SW_WL,  SW_ML, KC_G,    KC_DEL,                          KC_BSPC, KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
     SFT_CAP, KC_Z,    KC_X,  KC_C,   SW_SE, KC_B,    KC_LALT,                         KC_ALGR, KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_SLSH, KC_PGUP, \
     KC_LGUI, KC_LALT, KC_NO, EISU,          KC_LSFT, KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_RSFT,       RSTQ,     KC_NO,   KC_NO,   KC_PGDN  \
   ),
@@ -128,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MO] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,                            KC_F12,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______, \
     _______, _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,                           KC_NO , KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______, \
+    _______, _______, _______, _______, _______, TG_GA,   _______,                           KC_NO , KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______, \
     _______, _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______,          _______, _______,_______,          _______,_______, _______,          _______, _______, _______, _______  \
   ),
@@ -171,6 +175,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______,                          _______,   SEL_L,   SEL_D,   SEL_U,   SEL_R, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______,                          _______, _______,   SEL_H,   SEL_E, _______, _______, _______, \
     _______, _______, _______, _______,          _______, _______,_______,          _______,_______, _______,          _______, _______, _______, _______  \
+  ),
+
+  // Gaming layer 
+  // Same as querty but with the following differences
+  //  - no windows key
+  //  - arrow keys at the very right-bottom
+  //  - all other layers are deactivated but F (mothion layer) so I can still use F1~F12
+  [_GA] = LAYOUT(
+    KC_ESC , KC_1,    KC_2,  KC_3,  KC_4, KC_5,    KC_MINS,                         KC_EQL,  KC_6,   KC_7,  KC_8,     KC_9,    KC_0,    RSTQ,    \
+    KC_TAB , KC_Q,    KC_W,  KC_E,  KC_R, KC_T,    KC_LBRC,                         KC_RBRC, KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,    KC_NO,   \
+    KC_LCTL, KC_A,    KC_S,  KC_D,  KC_F, KC_G,    KC_DEL,                          KC_BSPC, KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V, KC_B,    KC_LALT,                         KC_ALGR, KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_UP,   KC_NO,   \
+    KC_NO,   KC_LALT, KC_NO, KC_NO,       KC_LSFT, KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_RSFT,       KC_NO,    KC_LEFT, KC_DOWN, KC_RIGHT \
   ),
 
   /* Adjust (not used)
