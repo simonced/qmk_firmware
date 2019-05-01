6052e696318e673b205d7ceea5d4b9b2ed04164e
Cedric Simon <5772359+simonced@users.noreply.github.com>
2019-05-01 18:35:42 +0900

    change the motion layer trigger on gaming layer to Enter

--- keyboards/ergodash/rev2/keymaps/simonced/keymap.c
+++ keyboards/ergodash/rev2/keymaps/simonced/keymap.c
@@ -36,7 +36,8 @@ extern keymap_config_t keymap_config;
 #define RSTQ TO(_QWERTY) // will force default layer
 
 // motion layer switch
-#define SW_ML LT(_MO, KC_F) // temporary
+#define SW_ML LT(_MO, KC_F) // temporary (on F key)
+#define SW_MLE LT(_MO, KC_ENT) // same on Enter
 
 // symbols layer switch
 // #define SW_SL LT(_SY, KC_ENT) // temporary (a bit painful for the whole day...)
@@ -180,13 +181,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Same as querty but with the following differences
   //  - no windows key
   //  - arrow keys at the very right-bottom
-  //  - all other layers are deactivated but F (motion layer) so I can still use F1~F12
+  //  - all other layers are deactivated but Enter (motion layer) so I can still use F1~F12
   [_GA] = LAYOUT(
     KC_ESC , KC_1,    KC_2,  KC_3,  KC_4,  KC_5,    KC_MINS,                         KC_EQL,  KC_6,   KC_7,  KC_8,     KC_9,    KC_0,    RSTQ,    \
     KC_TAB , KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,    KC_LBRC,                         KC_RBRC, KC_Y,   KC_U,  KC_I,     KC_O,    KC_P,    KC_NO,   \
-    KC_LCTL, KC_A,    KC_S,  KC_D,  SW_ML, KC_G,    KC_DEL,                          KC_BSPC, KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LCTL, KC_A,    KC_S,  KC_D,  KC_F,  KC_G,    KC_DEL,                          KC_BSPC, KC_H,   KC_J,  KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,    KC_LALT,                         KC_ALGR, KC_N,   KC_M,  KC_COMM,  KC_DOT,  KC_UP,   KC_NO,   \
-    KC_NO,   KC_LALT, KC_NO, KC_NO,        KC_LSFT, KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_RSFT,       KC_NO,    KC_LEFT, KC_DOWN, KC_RIGHT \
+    KC_NO,   KC_LALT, KC_NO, KC_NO,        KC_LSFT, KC_LCTL, KC_SPC,         SW_MLE, KC_RCTL, KC_RSFT,       KC_NO,    KC_LEFT, KC_DOWN, KC_RIGHT \
   ),
 
   /* Adjust (not used)
