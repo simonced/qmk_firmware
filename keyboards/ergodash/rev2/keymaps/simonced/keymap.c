1ee5059158c5a3b709deb8ce4cfc93b40fbcd8eb
Cedric Simon <5772359+simonced@users.noreply.github.com>
2021-11-02 10:41:03 +0900

    fix backslash key code

--- keyboards/ergodash/rev2/keymaps/simonced/keymap.c
+++ keyboards/ergodash/rev2/keymaps/simonced/keymap.c
@@ -156,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,     KC_F11,                               KC_F12,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______, \
     _______, KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,  KC_PERC,   KC_UNDS,                             KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, _______, _______, \
     _______, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,      KC_MINS ,                             KC_EQL, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_PIPE, _______, \
-    _______, KC_6,    KC_7,    KC_8,     KC_9,    KC_0,      _______ ,                            _______, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_NUBS, _______, \
+    _______, KC_6,    KC_7,    KC_8,     KC_9,    KC_0,      _______ ,                            _______, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_BSLS, _______, \
     _______, _______, _______, _______,           _______,   _______ ,_______,           _______, _______, _______,          _______,  KC_INS, KC_PSCR, _______  \
   ),
 
