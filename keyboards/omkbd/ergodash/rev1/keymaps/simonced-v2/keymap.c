8284d25421cef9a97b35102a52c5e2a67c021d33
Cedric Simon <5772359+simonced@users.noreply.github.com>
2023-11-25 13:31:48 +0900

    edit some keys, but doesn't compile, try to update from upstream

--- keyboards/omkbd/ergodash/rev1/keymaps/simonced-v2/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/simonced-v2/keymap.c
@@ -19,7 +19,8 @@ enum {
 #define LAY1 MO(LAYER_SYMB) // [MO]memtary layer (non assigned keys are "transparent")
 #define LAY2 TG(LAYER_NUM)  // [T]o[G]gle layer on/off (non assigned keys are "transparent")
 #define LAY3 TO(LAYER_GAME) // Go [TO] layer (all keys replaced)
-#define F_FUN LT(LAYER_SYMB, KC_F)
+#define F_FUN LT(LAYER_SYMB, KC_F) // K aslo as symbol layer temporary switch
+#define J_FUN LT(LAYER_SYMB, KC_J) // J aslo as symbol layer temporary switch
 
 // == SHORTCUTS RELATED ==
 
@@ -79,31 +80,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------------------------'      `--------------------'      `--------------------'      `---------------------------'
    */
   [LAYER_BASE] = LAYOUT( \
-    EISU ,   JP_1,   JP_2, JP_3, JP_4,  JP_5,    JP_MINS,                         JP_CIRC, JP_6, JP_7, JP_8,    JP_9,    JP_0,    KC_BSPC, \
-    KC_TAB , KC_Q,   KC_W, KC_E, KC_R,  KC_T,    JP_LBRC,                         JP_RBRC, KC_Y, KC_U, KC_I,    KC_O,    KC_P,    JP_AT,   \
-    CTR_ESC, KC_A,   KC_S, KC_D, F_FUN, KC_G,    KC_DEL,                          KC_BSPC, KC_H, KC_J, KC_K,    KC_L,    JP_SCLN, JP_COLN, \
-    SFT_CAP, KC_Z,   KC_X, KC_C, KC_V,  KC_B,    KC_LALT,                         KC_ALGR, KC_N, KC_M, JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, \
-    KC_LGUI, KC_LALT, LAY3, LAY2,      KC_LSFT,  KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_RSFT,    KC_MNXT, KC_MPLY, KC_VOLD, KC_VOLU  \
+    EISU ,   JP_1,   JP_2, JP_3, JP_4,  JP_5,    JP_MINS,                         JP_CIRC, JP_6, JP_7,  JP_8,    JP_9,    JP_0,    KC_BSPC, \
+    KC_TAB , KC_Q,   KC_W, KC_E, KC_R,  KC_T,    JP_LBRC,                         JP_RBRC, KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    JP_AT,   \
+    CTR_ESC, KC_A,   KC_S, KC_D, F_FUN, KC_G,    KC_DEL,                          KC_BSPC, KC_H, J_FUN, KC_K,    KC_L,    JP_SCLN, JP_COLN, \
+    SFT_CAP, KC_Z,   KC_X, KC_C, KC_V,  KC_B,    KC_LALT,                         KC_ALGR, KC_N, KC_M,  JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, \
+    KC_LGUI, KC_LALT, LAY3, LAY2,      KC_LSFT,  KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_RSFT,     KC_MNXT, KC_MPLY, KC_VOLD, KC_VOLU  \
   ),
 
   /*
-   * Motion +Symbols layer
+   * Motion +Symbols layer (overlay, so un-modified keys are from the base layer)
    * ,------------------------------------------------.                    ,------------------------------------------------.
    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |                    |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 | home |
    * |------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
    * |      |  !   |  "   |  #   |  $   |  %   |      |                    |      |  &   |  '   |  (   |  )   |  |   | end  |
    * |------+------+------+------+------+------+------|                    |------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |                    |      | LEFT | DOWN |  UP  | RGHT |      |      |
+   * |      |      |      |      |      |      |      |                    |      | LEFT | DOWN |  UP  | RGHT |  _   |      |
    * |------+------+------+------+------+------+------+------,      ,------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      |      |      |  {   |  }   |  [   |  ]   | pgup |
    * |-------------+------+------+------+------+------|      |      |      |------+------+------+------+------+-------------|
    * |      |      |      |      |      |      |      |      |      |      |      |      |      | pscr |  ins |      | pgdn |
    * `---------------------------'      `--------------------'      `--------------------'      `---------------------------'
    */
-  [LAYER_SYMB] = LAYOUT(
+  [LAYER_SYMB] = LAYOUT( \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,    KC_F11,                           KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_HOME, \
     _______, JP_EXLM, JP_DQUO, JP_HASH, JP_DLR, JP_PERC,  _______,                          _______, JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN,  JP_PIPE, KC_END,  \
-    _______, _______, _______, _______, _______, _______, _______,                          _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,                          _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, JP_UNDS, _______, \
     _______, _______, _______, _______, _______, _______, _______,                          _______, _______, JP_LCBR, JP_RCBR, JP_LBRC,  JP_RBRC, KC_PGUP, \
     _______, _______, _______, _______,          _______, _______,_______,          _______,_______, _______,          KC_PSCR, KC_INS,   _______, KC_PGDN  \
   ),
@@ -122,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |      |      |      |      |      |      |  0   |  .   | ent  |      |
    * `---------------------------'      `--------------------'      `--------------------'      `---------------------------'
    */
-  [LAYER_NUM] = LAYOUT(
+  [LAYER_NUM] = LAYOUT( \
     _______, _______, _______, _______, _______, _______, _______,                          _______, _______, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS, _______, \
     _______, _______, _______, _______, _______, _______, _______,                          _______, _______, KC_P7,    KC_P8,   KC_P9, KC_PPLS, _______, \
     _______, _______, _______, _______, _______, _______, _______,                          _______, _______, KC_P4,    KC_P5,   KC_P6, KC_PPLS, _______, \
@@ -148,12 +149,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |  L0  |      |      |      |      |      |      |      |      |      |      |  LFT |  DWN | RGHT |
    * `---------------------------'      `--------------------'      `--------------------'      `---------------------------'
    */
-  [LAYER_GAME] = LAYOUT(
+  [LAYER_GAME] = LAYOUT( \
     KC_ESC , KC_1,  KC_2,  KC_3, KC_4,  KC_5,    JP_MINS,                         JP_CIRC, KC_6,  KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC, \
     KC_TAB , KC_Q,  KC_W,  KC_E, KC_R,  KC_T,    JP_LBRC,                         JP_RBRC, KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    JP_AT,   \
     KC_LCTL, KC_A,  KC_S,  KC_D, KC_F,  KC_G,    KC_DEL,                          KC_BSPC, KC_H,  KC_J,  KC_K,    KC_L,    JP_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,  KC_X,  KC_C, KC_V,  KC_B,    KC_LALT,                         KC_ALGR, KC_N,  KC_M,  JP_COMM, JP_DOT,  KC_UP,   JP_SLSH, \
-    KC_NO, KC_LALT, LAY0, KC_NO,       KC_LSFT, KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_NO,        KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT \
+    KC_NO, KC_LALT, KC_NO, LAY0,        KC_LSFT, KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_RSFT,      KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT \
   ),
 };
 
