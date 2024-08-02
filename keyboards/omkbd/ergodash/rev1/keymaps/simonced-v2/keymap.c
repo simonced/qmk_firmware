8d810b7b86c2cd1be12a8c9295e7c492815d3e7b
Cedric Simon <5772359+simonced@users.noreply.github.com>
2024-08-02 21:58:32 +0900

    change F keys toggle on gaming layer

--- keyboards/omkbd/ergodash/rev1/keymaps/simonced-v2/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/simonced-v2/keymap.c
@@ -33,7 +33,7 @@ enum {
 #define MO_L2 MO(LAYER_NUM) // [MO]mentary layer Numbers + Music controls
 
 // activate some layers from gaming layer
-#define ENT_FUN LT(LAYER_SYMB2, KC_ENT) // Enter + functiion/motion layer (from gaming layer)
+#define F_KEYS MO(LAYER_SYMB2)
 
 // == SHORTCUTS RELATED ==
 
@@ -189,7 +189,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q,  KC_W,  KC_E, KC_R,  KC_T,    JP_LBRC,                         JP_RBRC, KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    JP_AT,   \
     KC_LCTL, KC_A,  KC_S,  KC_D, KC_F,  KC_G,    KC_DEL,                          KC_BSPC, KC_H,  KC_J,  KC_K,    KC_L,    JP_SCLN, JP_COLN, \
     KC_LSFT, KC_Z,  KC_X,  KC_C, KC_V,  KC_B,    KC_LALT,                         KC_ALGR, KC_N,  KC_M,  JP_COMM, JP_DOT,  KC_UP,   JP_SLSH, \
-    RST_G, KC_LALT, KC_NO, _______,     KC_LSFT, KC_LCTL, KC_SPC,        ENT_FUN, KC_RCTL, KC_RSFT,      KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT \
+    RST_G, KC_LALT, KC_NO, _______,     KC_LSFT, KC_LCTL, KC_SPC,         KC_ENT, KC_RCTL, KC_RSFT,      F_KEYS,  KC_LEFT, KC_DOWN, KC_RIGHT \
   ),
 
   /*
