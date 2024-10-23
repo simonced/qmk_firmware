6478ca24b74ff1a8c942066bf1d151830457853b
Cedric Simon <5772359+simonced@users.noreply.github.com>
2024-10-23 22:02:47 +0900

    fix adjust layer access

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/simonced/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/simonced/keymap.c
@@ -29,9 +29,9 @@ enum layers {
 #define SFT_BSP MT(MOD_LSFT, KC_BSPC)
 
 // eigo - lower
-#define ROMA_LW MT(MO(_LOWER), JP_MHEN)
+#define ROMA_LW LT(_LOWER, JP_MHEN)
 // kana - upper
-#define KANA_UP MT(MO(_RAISE), JP_HENK)
+#define KANA_UP LT(_RAISE, JP_HENK)
 
 // text selection (for _SE layer)
 #define SEL_L S(KC_LEFT)
@@ -54,18 +54,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_LOWER] = LAYOUT_ortho_4x12_1x2uC(
-    _______, JP_1, JP_2,    JP_3,    _______, _______, JP_CIRC, JP_PIPE,  JP_TILD, JP_LABK, JP_RABK, JP_EQL,
-    _______, JP_4, JP_5,    JP_6,    _______, _______, KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, JP_MINS, _______,
-    _______, JP_7, JP_8,    JP_9,    KC_MNXT, _______, _______,  _______, JP_SLSH, JP_BSLS, JP_QUES, ,
-    _______, JP_0, KC_VOLD, KC_VOLU, _______,      KC_MPLY,      _______, _______, _______, _______, _______
+    _______, JP_1, JP_2,    JP_3,    _______, _______, JP_CIRC, JP_PIPE,     JP_TILD, JP_LABK, JP_RABK, JP_EQL,
+    _______, JP_4, JP_5,    JP_6,    _______, _______, KC_LEFT, KC_DOWN,     KC_UP,   KC_RGHT, JP_MINS, _______,
+    _______, JP_7, JP_8,    JP_9,    KC_MNXT, _______, _______, _______,     JP_SLSH, JP_BSLS, JP_QUES, ,
+    _______, JP_0, KC_VOLD, KC_VOLU, _______,      KC_MPLY,     MO(_ADJUST), _______, _______, _______, _______
 ),
 
 
 [_RAISE] = LAYOUT_ortho_4x12_1x2uC(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______, JP_EXLM, JP_DQUO, JP_HASH, JP_DLR, JP_PERC,  JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_UNDS, KC_F12,
-    KC_CAPS, _______, _______, _______, _______, _______, _______, JP_LCBR, JP_RCBR, JP_LBRC, JP_RBRC, KC_DEL,
-    _______, KC_PSCR, KC_INS,  KC_BRK,  _______,      _______,     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,       KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    _______, JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,      JP_PERC, JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_UNDS, KC_F12,
+    KC_CAPS, _______, _______, _______, _______,     _______, _______, JP_LCBR, JP_RCBR, JP_LBRC, JP_RBRC, KC_DEL,
+    _______, KC_PSCR, KC_INS,  KC_BRK,  MO(_ADJUST),      _______,     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Adjust (Lower + Raise)
