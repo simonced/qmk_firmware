859d96d8bae9e88111d1561a65a9d1e5f8873d70
Cedric Simon <5772359+simonced@users.noreply.github.com>
2024-10-31 19:03:01 +0900

    added nav and sel layers
    can't live without those!

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/simonced/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/simonced/keymap.c
@@ -20,11 +20,13 @@ enum layers {
   _QWERTY,
   _LOWER,
   _RAISE,
+  _MOTI,
+  _SEL,
   _ADJUST
 };
 
 // ctrl and esc on the same key
-#define CTR_ESC MT(MOD_LCTL, KC_ESC)
+// #define CTR_ESC MT(MOD_LCTL, KC_ESC)
 // ctrl and space on the same key
 #define CTRL_SPC MT(MOD_LCTL, KC_SPC)
 
@@ -34,9 +36,10 @@ enum layers {
 // shift and enter on the same key
 // #define SFT_ENT MT(MOD_LSFT, KC_ENT)
 
-
-// F or lower
-#define F_LOWER LT(_LOWER, KC_F)
+// F or _MOTI
+#define F_MOTI LT(_MOTI, KC_F)
+// V or _SEL
+#define V_SEL  LT(_SEL,  KC_V)
 // eigo - lower
 #define ROMA_LW LT(_LOWER, JP_MHEN)
 // kana - upper
@@ -56,25 +59,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_ortho_4x12_1x2uC(
     KC_TAB,   KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,   KC_A,    KC_S,    KC_D,  F_LOWER, KC_G,    KC_H, KC_J,    KC_K,    KC_L,    JP_SCLN, KC_ENT,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N, KC_M,    JP_COMM, JP_DOT,  JP_SLSH, KC_LSFT,
+    KC_ESC,   KC_A,    KC_S,    KC_D,  F_MOTI,  KC_G,    KC_H, KC_J,    KC_K,    KC_L,    JP_SCLN, KC_ENT,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,  V_SEL,   KC_B,    KC_N, KC_M,    JP_COMM, JP_DOT,  JP_SLSH, KC_LSFT,
     KC_LCTL,  KC_LGUI, KC_LALT, KC_NO, ROMA_LW,   CTRL_SPC,    KANA_UP, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
-
 [_LOWER] = LAYOUT_ortho_4x12_1x2uC(
     _______, JP_1, JP_2,    JP_3,    _______, _______, JP_CIRC, JP_PIPE,     JP_TILD, JP_LABK, JP_RABK, JP_EQL,
-    _______, JP_4, JP_5,    JP_6,    _______, _______, KC_LEFT, KC_DOWN,     KC_UP,   KC_RGHT, JP_MINS, JP_COLN,
-    _______, JP_7, JP_8,    JP_9,    KC_MNXT, _______, _______, _______,     _______, _______, JP_BSLS, JP_ASTR,
-    _______, JP_0, KC_VOLD, KC_VOLU, _______,      KC_MPLY,     MO(_ADJUST), _______, _______, _______, _______
+    _______, JP_4, JP_5,    JP_6,    _______, _______, _______, _______,     _______, _______, JP_MINS, JP_COLN,
+    _______, JP_7, JP_8,    JP_9,    KC_MNXT, KC_MPLY, _______, _______,     _______, _______, JP_BSLS, JP_ASTR,
+    _______, JP_0, KC_VOLD, KC_VOLU, _______,      _______,     MO(_ADJUST), _______, _______, _______, _______
 ),
-
 
 [_RAISE] = LAYOUT_ortho_4x12_1x2uC(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,       KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
     JP_ZKHK, JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,      JP_PERC, JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, JP_UNDS, _______,
     KC_CAPS, JP_LCBR, JP_RCBR, JP_LBRC, JP_RBRC,     _______, _______, JP_AT,   JP_GRV,   _______, _______, KC_DEL,
     _______, KC_PSCR, KC_INS,  KC_BRK,  MO(_ADJUST),      _______,     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+),
+
+[_MOTI] = LAYOUT_ortho_4x12_1x2uC(
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, KC_PGUP, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_HOME,  KC_PGDN,  _______, KC_PGDN, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+),
+
+[_SEL] = LAYOUT_ortho_4x12_1x2uC(
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, SEL_L,   SEL_D,    SEL_U,    SEL_R,   KC_NO, _______,
+    _______, _______, _______, _______, _______, _______, KC_NO,   SEL_H,    SEL_E,    KC_NO,   KC_NO, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
